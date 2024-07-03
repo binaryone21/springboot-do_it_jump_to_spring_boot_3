57285ef628415cca84b21c68094d44e2f1d31bf3
seungheejo <binaryone21@gmail.com>
2024-07-03 21:39:54 +0900

    02. 05. 09. 답변 데이터를 통해 질문 데이터 찾기 VS 질문 데이터를 통해 답변 찾기

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -110,5 +111,18 @@ class SbbApplicationTests {
         assertTrue(oa.isPresent());
         Answer a = oa.get();
         assertEquals(2, a.getQuestion().getId());
+    }
+
+    @Transactional
+    @Test
+    void findQuestionByAnswer_vs_findAnswerByQuestion() {
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
 }
